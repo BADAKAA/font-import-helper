7216bd7ee94559796d742507dab5bad5f011347d
WN00163949 <bassit.agbere@wuerth-it.com>
2021-11-04 02:53:31 +0100

    fix bug: success message not diappearing

--- script.js
+++ script.js
@@ -96,7 +96,7 @@ function showMessage(message) {
     errorEl.textContent = '';
     successEl.textContent = message;
     clearTimeout(successTimer);
-    successTimer = setTimeout(() => errorEl.textContent = '', 4000);
+    successTimer = setTimeout(() => successEl.textContent = '', 4000);
 }
 function parsePrefix(prefix, font) {
     if (!prefix.includes('{')) return prefix;
